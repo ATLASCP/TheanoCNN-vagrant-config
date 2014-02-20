ee65fd4f69679aba8e2ed629a4551f6874c06f1d
ddetone <daniel.detone@gmail.com>
2014-02-20 13:57:11 -0500

    update theano and python libs to most recent

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,18 +7,57 @@
 # To display detailed information when executing this script
 set -ex
 
-# Update apt-get
+###### Apt-Get #########
+
+# PTI stands for Programs To Install
+PTI=''
+
+# Git
+PTI="${PTI} git"
+
+# Vim
+PTI="${PTI} vim"
+
+# Python packages and requirements
+PTI="${PTI} curl zlib1g-dev libbz2-dev libreadline-dev libgdbm-dev libssl-dev \
+libsqlite3-dev python python-setuptools python-dev python-numpy python-scipy python-matplotlib \
+ipython ipython-notebook python-pandas python-sympy python-nose \
+g++ python-software-properties libopenblas-base libopenblas-dev liblapack-dev \
+gfortran libfreetype6-dev"
+
+# Remove libatlas because Theanos requires blas, and it is faster
+apt-get --purge -y remove libatlas3gf-base libatlas-dev
+
+# Bring apt-get up to date
 apt-get update
 
-# Generic tools
-apt-get install -y git
-apt-get install -y vim
-apt-get install -y build-essential
+# Install all the apt-get packages
+apt-get install -y ${PTI}
 
-# Theano dependencies (Python Math Toolbox)
-apt-get install -y python-numpy python-scipy python-dev python-pip python-nose
-apt-get install -y libopenblas-dev
-pip install Theano
+####### Python ##########  
+   
+# Upgrade older Ubuntu packages to most recent PyPi versions
+# Sequence matters
+easy_install pip
+pip install --upgrade setuptools --no-use-wheel
+pip install --upgrade pip
+pip install --upgrade cython
+
+# Install scimath and numpy, these components are at the bottom
+# as they tend to cause problems and should crash at the end to avoid reprovisioning
+pip install --upgrade numpy
+/usr/local/bin/easy_install scimath
+pip install --upgrade ipython[all] jinja2 vincent virtualenv pythonbrew pandas SciPy matplotlib
+pip install --upgrade theano
+
+# Switch symlink from libatlas to libopenblas
+# Manually this would be done with commmand
+# update-alternatives --config libblas.so.3gf < Option 2: /usr/lib/lapack/liblapack.so.3gf 
+rm -rf /usr/lib/liblapack.so.3gf
+ln -s /usr/lib/lapack/liblapack.so.3gf /usr/lib/liblapack.so.3gf
+
+# Insert cludge to fix Theano imports
+sed -i '130iimport numpy.distutils.__config__' /usr/local/lib/python2.7/dist-packages/theano/tensor/blas.py
 
 
 # Add sync folder
